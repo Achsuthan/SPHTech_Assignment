895a34a042764754f77a08ac185f129c7701b92f
Achsuthan <achsuthan@icloud.com>
2020-08-18 00:06:09 +0530

    Pod file added Dummy images added

---
+++ SPHTech_Assignment/Model/DataConsumptionModel.swift
@@ -0,0 +1,26 @@
+//
+//  DataConsumptionModel.swift
+//  SPHTech_Assignment
+//
+//  Created by Achsuthan Mahendran on 8/17/20.
+//  Copyright © 2020 Achsuthan Mahendran. All rights reserved.
+//
+
+import Foundation
+
+struct DataConsumptionModel:Codable {
+    var records: [Records]
+    var _links: Links
+    var offset: Int? = 0
+    var total: Int? = 0
+}
+
+struct Records:Codable {
+    var _id: Int
+    var volume_of_mobile_data: String
+    var quarter: String
+}
+
+struct Links:Codable {
+    var next: String?
+}
