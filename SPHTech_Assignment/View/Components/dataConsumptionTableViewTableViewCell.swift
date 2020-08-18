e47ec2c6dd2fdaf2ec003a89881b53942ad87f87
Achsuthan <achsuthan@icloud.com>
2020-08-18 18:29:27 +0530

    1. Clickable large image added with logic 2. UI modified

--- SPHTech_Assignment/View/Components/dataConsumptionTableViewTableViewCell.swift
+++ SPHTech_Assignment/View/Components/dataConsumptionTableViewTableViewCell.swift
@@ -20,6 +20,7 @@ class dataConsumptionTableViewTableViewCell: UITableViewCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Year: 2020"
+        label.numberOfLines = 0
         return label
     }()
     
@@ -27,24 +28,28 @@ class dataConsumptionTableViewTableViewCell: UITableViewCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Usage: 0.23232"
+        label.numberOfLines = 0
         return label
     }()
     
     let img: UIImageView = {
         let image = UIImageView()
-        image.image = #imageLiteral(resourceName: "select")
+        image.image = #imageLiteral(resourceName: "viewMore")
         image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
         return image
     }()
     
     var delegate: dataConsumtionImagclick!
     var index: Int = 0 {
         didSet{
-            if self.index > -1 {
-                self.img.alpha = 1
-            }
-            else {
-                self.img.alpha = 0
+            DispatchQueue.main.async {
+                if self.index > -1 {
+                    self.img.alpha = 1
+                }
+                else {
+                    self.img.alpha = 0
+                }
             }
         }
     }
@@ -67,23 +72,23 @@ class dataConsumptionTableViewTableViewCell: UITableViewCell {
         self.viewBackground.heightAnchor.constraint(greaterThanOrEqualToConstant: 80).isActive = true
         
         self.viewBackground.addSubview(self.img)
+        self.img.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        self.img.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
         self.img.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20).isActive = true
-        self.img.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        self.img.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        self.img.centerYAnchor.constraint(equalTo: self.viewBackground.centerYAnchor).isActive = true
+        self.img.heightAnchor.constraint(equalToConstant:220).isActive = true
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.imgClicked(gesture:)))
         img.addGestureRecognizer(tapGesture)
         img.isUserInteractionEnabled = true
         
         self.viewBackground.addSubview(self.lblYear)
-        self.lblYear.topAnchor.constraint(equalTo: self.viewBackground.topAnchor, constant: 10).isActive = true
+        self.lblYear.topAnchor.constraint(equalTo: self.img.bottomAnchor, constant: 10).isActive = true
         self.lblYear.leadingAnchor.constraint(equalTo: self.viewBackground.leadingAnchor, constant: 10).isActive = true
-        self.lblYear.trailingAnchor.constraint(equalTo: self.img.trailingAnchor, constant: -10).isActive = true
+        self.lblYear.trailingAnchor.constraint(equalTo: self.viewBackground.trailingAnchor, constant: -10).isActive = true
         
         self.viewBackground.addSubview(self.lblDataUsage)
         self.lblDataUsage.topAnchor.constraint(equalTo: self.lblYear.bottomAnchor, constant: 10).isActive = true
         self.lblDataUsage.leadingAnchor.constraint(equalTo: self.viewBackground.leadingAnchor, constant: 10).isActive = true
-        self.lblDataUsage.trailingAnchor.constraint(equalTo: self.img.trailingAnchor, constant: -10).isActive = true
+        self.lblDataUsage.trailingAnchor.constraint(equalTo: self.viewBackground.trailingAnchor, constant: -10).isActive = true
         self.lblDataUsage.bottomAnchor.constraint(equalTo: self.viewBackground.bottomAnchor, constant: -10).isActive = true
     }
     
